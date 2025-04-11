2e3a063e6c6238f6cf66100c9218003618e99830
Vacheprime <danatmuradov500@gmail.com>
2025-04-10 22:03:48 -0400

    Add .metadata for version control.

--- lib/model/User.dart
+++ lib/model/User.dart
@@ -5,7 +5,7 @@ class User {
   String email;
   String phone;
   String password;
-  Map<String, dynamic> settings;
+  UserSettings settings;
 
   User ({
     required this.id,
@@ -15,4 +15,48 @@ class User {
     required this.password,
     required this.settings,
   });
+}
+
+/// UserSettings class is used to represent the app settings of a user.
+class UserSettings {
+  late bool _isDarkMode;
+  late bool _enabledNotifications;
+  late bool _enabledLocationServices;
+
+  /// Default constructor that can accept any settings.
+  UserSettings({bool isDarkMode = false, bool enabledNotifications = false, bool enabledLocation = false}) {
+    _isDarkMode = isDarkMode;
+    _enabledNotifications = enabledNotifications;
+    _enabledLocationServices = enabledLocation;
+  }
+
+  /// Toggle dark mode from true to false and vice versa.
+  void toggleDarkMode() {
+    _isDarkMode = !_isDarkMode;
+  }
+
+  /// Toggle enabled notifications from true to false and vice versa.
+  void toggleNotifications() {
+    _enabledNotifications = !_enabledNotifications;
+  }
+
+  /// Toggle enabled location services from true to false and vice versa.
+  void toggleLocationServices() {
+    _enabledLocationServices = !_enabledLocationServices;
+  }
+
+  /// Getter for _isDarkMode.
+  bool isDarkModeEnabled() {
+    return _isDarkMode;
+  }
+
+  /// Getter _enabledNotifications.
+  bool areNotificationsEnabled() {
+    return _enabledNotifications;
+  }
+
+  /// Getter for _enabledLocationServices.
+  bool areLocationServicesEnabled() {
+    return _enabledLocationServices;
+  }
 }
--- lib/services/firebase/firebasemanager.dart
+++ lib/services/firebase/firebasemanager.dart
@@ -1,8 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
+/// FirebaseManager class is used to define Firebase/Firestore specific operations.
 class FirebaseManager {
+
+  /// Initialize the Firebase application with the appropriate arguments.
   static Future<void> initFirebase() async {
     await Firebase.initializeApp(
         options: const FirebaseOptions(

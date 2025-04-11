caac1f0eef74e4990921cde420254f7a98cf25f5
AndrewShahini <shahiniandrew@gmail.com>
2025-04-11 10:31:35 -0400

    Update main.dart
    added the stateless widget to the main class

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:munchit/view/splashscreen.dart';
 
 void main() {
-  runApp(Splash());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Splash(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }

ec944cf1c3334858ac7046a97397068e1065adcd
AndrewShahini <shahiniandrew@gmail.com>
2025-04-12 17:10:57 -0400

    added scaffold
    added scaffolds

---
+++ lib/view/accountpage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Account extends StatefulWidget {
+  const Account({super.key});
+
+  @override
+  State<Account> createState() => _AccountState();
+}
+
+class _AccountState extends State<Account> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {
+        }, icon: Icon(Icons.menu)),
+        title: Text("Munch't"),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {
+            Navigator.of(context).pop();
+          }, icon: Icon(Icons.arrow_back))
+        ],
+      ),
+    );
+  }
+}

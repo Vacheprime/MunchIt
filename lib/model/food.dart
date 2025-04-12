de6e3388a542098008b686333a6322614ae6ba42
Vacheprime <danatmuradov500@gmail.com>
2025-04-12 14:18:17 -0400

    Implemented field validation methods, setters, getters, and the constructor for the Food class.

--- lib/model/food.dart
+++ lib/model/food.dart
@@ -1,26 +1,141 @@
+import 'package:munchit/services/utils/utils.dart';
 import 'review.dart';
 
+/// Food class is used to represent a menu item of a restaurant.
 class Food {
-  // Information
-  int id;
-  String name;
-  double price;
-  Map<String, String> allergies;
-  List<String> image;
-  // Statistics
-  int likes;
-  int saves;
-  List<Review> reviews;
+  String? _docId;
+  late String _name;
+  late double _price;
+  late String _image;
+  late String _allergies;
+  bool _hasNuts = false;
+  bool _hasPeanuts = false;
+  int _likes = 0;
+  int _saves = 0;
+  final List<Review> _reviews = [];
 
+  Food(String name, double price, String image, {bool hasNuts = false, bool hasPeanuts = false, String allergies = ""}) {
+    setName(name);
+    setPrice(price);
+    setImage(image);
+    setAllergies(allergies);
+    _hasNuts = hasNuts;
+    _hasPeanuts = hasPeanuts;
+  }
 
-  Food ({
-    required this.id,
-    required this.name,
-    required this.price,
-    required this.allergies,
-    required this.image,
-    required this.likes,
-    required this.saves,
-    required this.reviews,
-  });
+  String? getDocId() {
+    return _docId;
+  }
+
+  void setDocId(String docId) {
+    _docId = docId;
+  }
+
+  String getName() {
+    return _name;
+  }
+
+  void setName(String name) {
+    if (!validateName(name)) throw ArgumentError("The name is invalid!");
+    _name = name;
+  }
+
+  double getPrice() {
+    return _price;
+  }
+
+  void setPrice(double price) {
+    if (!validatePrice(price)) throw ArgumentError("The price is invalid!");
+    _price = price;
+  }
+
+  String getImage() {
+    return _image;
+  }
+
+  void setImage(String image) {
+    _image = image;
+  }
+
+  String getAllergies() {
+    return _allergies;
+  }
+
+  void setAllergies(String allergies) {
+    if (!validateAllergies(allergies)) throw ArgumentError("The allergies are invalid!");
+    _allergies = allergies;
+  }
+
+  bool hasNuts() {
+    return _hasNuts;
+  }
+
+  void setHasNuts(bool hasNuts) {
+    _hasNuts = hasNuts;
+  }
+
+  bool hasPeanuts() {
+    return _hasPeanuts;
+  }
+
+  void setHasPeanuts(bool hasPeanuts) {
+    _hasPeanuts = hasPeanuts;
+  }
+
+  int getLikes() {
+    return _likes;
+  }
+
+  void addLike() {
+    _likes++;
+  }
+
+  void removeLike() {
+    _likes--;
+  }
+
+  int getSaves() {
+    return _saves;
+  }
+
+  void addSave() {
+    _saves++;
+  }
+
+  void removeSave() {
+    _saves--;
+  }
+
+  List<Review> getReviews() {
+    return _reviews;
+  }
+
+  void addReview(Review review) {
+    _reviews.add(review);
+  }
+
+  void removeReview(Review review) {
+    _reviews.remove(review);
+  }
+
+  double getRating() {
+    return _reviews.map((review) => review.getRating()).reduce((r1, r2) => r1 + r2) / _reviews.length;
+  }
+
+  static bool validateName(String name) {
+    if (Utils.hasInvalidSpaces(name)) return false;
+    RegExp validNameRegex = RegExp(r"^[\p{L} ]+$", unicode: true);
+    return validNameRegex.hasMatch(name);
+  }
+
+  static bool validatePrice(double price) {
+    return price > 0;
+  }
+
+  static bool validateAllergies(String allergies) {
+    if (allergies.isEmpty) return true;
+    if (Utils.hasInvalidSpaces(allergies)) return false;
+    RegExp validAllergiesRegex = RegExp(r"^[\p{L},; ]$", unicode: true);
+    return validAllergiesRegex.hasMatch(allergies);
+  }
 }

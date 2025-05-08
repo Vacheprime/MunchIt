4947e5dfa32102111632e2ad1f29242d1c69a3b3
Vacheprime <danatmuradov500@gmail.com>
2025-05-08 12:51:07 -0400

    Models [Repositories/UserRepository]: Implemented fetching users.

--- lib/services/repositories/user_repository.dart
+++ lib/services/repositories/user_repository.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:munchit/model/user.dart';
 import 'package:munchit/services/exceptions/FirestoreInsertException.dart';
 import 'package:munchit/services/repositories/base_repository.dart';
+import 'package:munchit/services/repositories/food_repository.dart';
+import 'package:munchit/services/repositories/restaurant_repository.dart';
+import 'package:munchit/services/repositories/review_repository.dart';
 
 final class UserRepository extends BaseRepository<UserRepository> {
   static const String collectionName = "users";
@@ -58,6 +61,43 @@ final class UserRepository extends BaseRepository<UserRepository> {
     }
   }
 
+  Future<List<User>> _mapFromSnapshots(List<DocumentSnapshot> docs) async {
+    List<User> users = [];
+    for (DocumentSnapshot doc in docs) {
+      // Get the data
+      Map<String, dynamic> data = doc.data()! as Map<String, dynamic>;
+
+      // Instantiate the repositories
+      RestaurantRepository restaurantRepository = RestaurantRepository();
+      FoodRepository foodRepository = FoodRepository();
+      ReviewRepository reviewRepository = ReviewRepository();
+
+      // Get the liked restaurants
+      List<String> likedRestaurantIds = List<String>.from(data["likedRestaurants"]);
+      data["likedRestaurants"] = await restaurantRepository.getFromIds(likedRestaurantIds);
+
+      // Get the saved restaurants
+      List<String> savedRestaurantIds = List<String>.from(data["savedRestaurants"]);
+      data["savedRestaurants"] = await restaurantRepository.getFromIds(savedRestaurantIds);
+
+      // Get the created restaurants
+      List<String> createdRestaurantIds = List<String>.from(data["createdRestaurants"]);
+      data["createdRestaurants"] = await restaurantRepository.getFromIds(createdRestaurantIds);
+
+      // Get the created foods
+      List<String> createdFoodIds = List<String>.from(data["createdFoods"]);
+      data["createdFoods"] = await foodRepository.getFromIds(createdFoodIds);
+
+      // Get the created reviews
+      List<String> createdReviews = List<String>.from(data["createdReviews"]);
+      data["createdReviews"] = await reviewRepository.getFromIds(createdReviews);
+
+      // Add the user to the users list
+      users.add(User.fromFirebase(doc.id, data));
+    }
+    return users;
+  }
+
   /// Retrieve the results of the current query of the
   /// UserRepository.
   @override
@@ -65,16 +105,10 @@ final class UserRepository extends BaseRepository<UserRepository> {
     // Get the query
     RepositoryQuery query = getQueryClone();
     // Fetch the data
-    QuerySnapshot<Map<String, dynamic>> snapshot = await query.get();
+    QuerySnapshot snapshot = await query.get();
     // Get the documents
-    List<QueryDocumentSnapshot<Map<String, dynamic>>> docs = snapshot.docs;
-    // Get all users
-    List<User> results = [];
-    for (int i = 0; i < docs.length; i++) {
-      QueryDocumentSnapshot<Map<String, dynamic>> snapshot = docs[i];
-      results.add(User.fromFirebase(snapshot.id, snapshot.data()));
-    }
-    return results;
+    List<QueryDocumentSnapshot> docs = snapshot.docs;
+    return await _mapFromSnapshots(docs);
   }
 
   /// Clone the UserRepository.

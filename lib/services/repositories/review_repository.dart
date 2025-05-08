bc7436d5a2a275e4807a1a2935c8dad28159d396
Vacheprime <danatmuradov500@gmail.com>
2025-05-07 21:58:15 -0400

    Added add() and getFromIds() methods.

--- lib/services/repositories/review_repository.dart
+++ lib/services/repositories/review_repository.dart
@@ -12,9 +12,31 @@ final class ReviewRepository extends BaseRepository<ReviewRepository> {
 
   ReviewRepository._fromFilter(RepositoryQuery query, List<Transformation> transformations): super.fromFilter(collectionName, query, transformations);
 
-  Future<List<Review>> getFromIds(int docId) async {
+  Future<void> add(Review review) async {
+    // Get the collection reference
+    CollectionReference collection = getCollectionReference();
+    try {
+      // Insert the review and set its document ID
+      DocumentReference ref = await collection.add(review.toMap());
+      review.setDocId(ref.id);
+    } catch (e) {
+      throw FirestoreInsertException("An error occurred during the insertion"
+          "of the review.", e);
+    }
+  }
 
-
+  Future<List<Review>> getFromIds(List<String> docIds) async {
+    CollectionReference collection = getCollectionReference();
+    List<Review> reviews = [];
+    for (int i = 0; i < docIds.length; i++) {
+      // Fetch review document
+      DocumentSnapshot document = await collection.doc(docIds[i]).get();
+      // Create review object
+      Review review = Review.fromFirebase(document.id, document.data()! as Map<String, dynamic>);
+      // Add to reviews list
+      reviews.add(review);
+    }
+    return reviews;
   }
 
   @override

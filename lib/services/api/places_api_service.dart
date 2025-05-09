cb5d300c33b0bc3da1575086500f7f388574600b
Vacheprime <danatmuradov500@gmail.com>
2025-05-08 22:45:30 -0400

    api [PlacesAPIService]: Implemented searching for a restaurant by name or address and getting details.

--- lib/services/api/places_api_service.dart
+++ lib/services/api/places_api_service.dart
@@ -11,6 +11,22 @@ class PlacesApiService {
     _loadFromApiKeyFile();
   }
 
+  Future<List<PlacesSearchResult>> findRestaurantsNearby(Location location, int radius, {String? name, String? address}) async {
+    if (name == null && address == null) {
+      throw ArgumentError("The name or the address must be specified.");
+    }
+    String query = address ?? name!;
+    return (await _places.searchNearbyWithRadius(location, radius, name: query, type: "restaurant")).results;
+  }
+
+  Future<PlaceDetails?> getRestaurantDetails(String placeId) async {
+    PlacesDetailsResponse detailsResponse = await _places.getDetailsByPlaceId(placeId);
+    if (detailsResponse.hasNoResults) {
+      return null;
+    }
+    return detailsResponse.result;
+  }
+
   void _loadFromApiKeyFile() {
     File file = File("lib/services/api/config/$apiKeyFile");
     try {

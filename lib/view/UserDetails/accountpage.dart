4008be7df98b2722ced3769aedc9a9d903a32056
Vacheprime <danatmuradov500@gmail.com>
2025-05-09 21:04:50 -0400

    Views [AccountPage]: Fixed logout not clearing the stored credentials.

--- lib/view/UserDetails/accountpage.dart
+++ lib/view/UserDetails/accountpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:munchit/services/repositories/user_repository.dart';
+import 'package:munchit/view/Login/homepage.dart';
 import 'package:munchit/view/UserDetails/changepasswordpage.dart';
 import 'package:munchit/view/UserDetails/userstatspage.dart';
 import '../../model/user.dart';
@@ -14,7 +16,6 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
-
   TextEditingController username = TextEditingController();
   TextEditingController email = TextEditingController();
   TextEditingController phone = TextEditingController();
@@ -38,110 +39,126 @@ class _AccountState extends State<Account> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(248, 145, 145, 1),
-        leading: IconButton(onPressed: () {
-          Navigator.of(context).pop();
-        }, icon: Icon(Icons.arrow_back)),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.arrow_back)),
         title: Text("Munch't"),
         centerTitle: true,
       ),
       body: Center(
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        SizedBox(height: 20),
-        Text("Account", style: TextStyle(fontSize: 24)),
-        SizedBox(height: 15),
-
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: email,
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    border: OutlineInputBorder(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            Text("Account", style: TextStyle(fontSize: 24)),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: email,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(height: 25,),
-
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: phone,
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    labelText: "Phone Number",
-                    border: OutlineInputBorder(),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: phone,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        labelText: "Phone Number",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(height: 25,),
-
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: username,
-                  readOnly: !isEditing,
-                  decoration: InputDecoration(
-                    labelText: "Username",
-                    border: OutlineInputBorder(),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: username,
+                      readOnly: !isEditing,
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
+                  IconButton(
+                    icon: Icon(isEditing ? Icons.check : Icons.edit),
+                    onPressed: () {
+                      setState(() {
+                        if (isEditing) {
+                          //update functionality from firebase
+                        }
+                        isEditing = !isEditing;
+                      });
+                    },
+                  )
+                ],
               ),
-              IconButton(
-                icon: Icon(isEditing ? Icons.check : Icons.edit),
-                onPressed: () {
-                  setState(() {
-                    if (isEditing) {
-                        //update functionality from firebase
-                    }
-                    isEditing = !isEditing;
-                  });
-                },
-              )
-            ],
-          ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ChangePassword(
+                              user: widget.user,
+                            )));
+              },
+              child: Text(
+                "Change Password",
+                style: TextStyle(color: Color.fromRGBO(248, 145, 145, 1)),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _logout,
+              child: Text(
+                "Log out",
+                style: TextStyle(color: Color.fromRGBO(248, 145, 145, 1)),
+              ),
+            ),
+          ],
         ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => ChangePassword(user: widget.user,)));
-          },
-          child: Text(
-            "Change Password",
-            style: TextStyle(color: Color.fromRGBO(248, 145, 145, 1)),
-          ),
-        ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.popUntil(context, (route) => route.isFirst);
-          },
-          child: Text(
-            "Log out",
-            style: TextStyle(color: Color.fromRGBO(248, 145, 145, 1)),
-          ),
-        ),
-      ],
-    ),
-    ),
+      ),
     );
+  }
+
+  Future<void> _logout() async {
+    UserRepository userRepository = UserRepository();
+    await userRepository.eraseCredentials();
+    if (!mounted) return;
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (context) => Homepage(),
+        ),
+        (route) => false);
   }
 }
